093be511355994d406ebd41a84b212748110ae05
aayushshetty12 <aayush.shetty12@gmail.com>
2021-11-09 18:48:07 +0530

    add keys

---
+++ src/keys.js
@@ -0,0 +1,6 @@
+const keys={
+  geocode:'pk.eyJ1Ijoicm9zaGFuc2luZ2hhIiwiYSI6ImNrOWpuaWVnZTA5d2szZ3A4ajVoeGI2NG8ifQ.8y1ghqjMEU85dQquPxPp5A',
+  weatherstack:'8d59c3edfee8f0100b9951cea20ed965'
+}
+
+export default keys;
